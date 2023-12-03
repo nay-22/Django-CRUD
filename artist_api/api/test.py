1ed3023e9918f84a08b19830b6cffc04a5bba698
Nayan <nayan92001@gmail.com>
2023-12-03 14:25:35 +0530

    'minor'

--- artist_api/api/test.py
+++
@@ -1,8 +0,0 @@
-from models import Artist
-
-# Retrieve all objects
-all_artists = Artist.objects.all()
-
-# Print the objects
-for artist in all_artists:
-    print(artist.name, artist.user.username, artist.works.all())
